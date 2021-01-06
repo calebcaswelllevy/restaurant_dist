8f968e8d4479c1ba667abb0c85742b9fa033210d
Caleb Caswell-Levy <calebcaswelllevy@gmail.com>
2021-01-05 19:33:07 -0800

    more testing

--- main.js
+++ main.js
@@ -28,7 +28,7 @@
         description.id = 'description';
         description.textContent = ('El Tarasco is a South Bay classic that has been serving up Michoacán style Mexican food since 1965. Lots of things have changed in Manhattan Beach since then, but Tarasco is holding down the fort. This page is a tribute to my favorite Mexican place since I was a kid. I recommend the Jr. Super Deluxe with red and green mole.');
         title.textContent = 'About';
-        image.setAttribute('src', './images/storefront.png');
+        image.setAttribute('src', 'images/storefront.png');
         body.appendChild(title);
         body.appendChild(description);
         body.appendChild(image);
