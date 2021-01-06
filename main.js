fa72be21c16f59805580f4cbdde7c646ea1d3abc
Caleb Caswell-Levy <calebcaswelllevy@gmail.com>
2021-01-05 19:42:54 -0800

    testing

--- main.js
+++ main.js
@@ -63,11 +63,11 @@
         var title = document.createElement('h1');
         //Add images of the menu:
         var menuFront = document.createElement('img');
-        menuFront.setAttribute('src', '/images/Menufront.jpg');
+        menuFront.setAttribute('src', 'images/Menufront.jpg');
         menuFront.setAttribute('alt', 'An image of the front of the menu');
         menuFront.classList.add('menu');
         var menuBack = document.createElement('img');
-        menuBack.setAttribute('src', '/images/menuBack.jpg');
+        menuBack.setAttribute('src', 'images/menuBack.jpg');
         menuBack.setAttribute('alt', 'An image of the back of the menu');
         menuBack.classList.add('menu');
         //Add it to the DOM
